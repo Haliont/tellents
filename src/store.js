85e4baa7bdac5e85f39843667998e71acd2ffe53
Zemliakov Konstantin <zemliakov.konstantin@gmail.com>
2018-11-20 14:49:11 +0600

    Connected redux-thunck

--- src/store.js
+++ src/store.js
@@ -1,7 +1,18 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
-export default createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() // eslint-disable-line
+/* eslint-disable */
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
+/* eslint-enable */
+
+const middleware = [thunk];
+
+const enhancer = composeEnhancers(
+  applyMiddleware(...middleware),
 );
+
+export default createStore(rootReducer, enhancer);
