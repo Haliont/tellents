d8a06c0b847cd94bea35174aec31fab652e72dea
Zemliakov Konstantin <zemliakov.konstantin@gmail.com>
2018-11-23 14:26:37 +0600

    Fixed reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,15 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
+
 import signingUpState from './signingUpState';
 import signingInState from './signingInState';
 import validationTokenState from './validationTokenState';
 import isLoggedIn from './isLoggedIn';
+import user from './user';
 
 export default combineReducers({
   form: formReducer,
+  user,
   isLoggedIn,
   signingUpState,
   signingInState,
--- src/reducers/isLoggedIn.js
+++ src/reducers/isLoggedIn.js
@@ -1,9 +1,17 @@
 import { handleActions, combineActions } from 'redux-actions';
-import { signInSuccess } from './signingInState';
-import { signUpSuccess } from './signingUpState';
+import { signInSuccess, signInFailure } from './signingInState';
+import { signUpSuccess, signUpFailure } from './signingUpState';
+import { validateTokenFailure } from './validationTokenState';
 
 export default handleActions({
   [combineActions(signInSuccess, signUpSuccess)]() {
     return true;
+  },
+  [combineActions(
+    signInFailure,
+    signUpFailure,
+    validateTokenFailure,
+  )]() {
+    return false;
   },
 }, false);
