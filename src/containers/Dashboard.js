07d5cd3e2b4aeb03e82b9d181f04602e7b4f34e8
Zemliakov Konstantin <zemliakov.konstantin@gmail.com>
2018-11-26 16:03:19 +0600

    Made Dashboard container

---
+++ src/containers/Dashboard.js
@@ -0,0 +1,11 @@
+import { connect } from 'react-redux';
+import Component from '../components/Dashboard';
+
+// import { actionCreator } from '../reducers/user';
+
+const mapStateToProps = () => ({});
+
+export default connect(
+  mapStateToProps,
+  // { actionCreator },
+)(Component);
