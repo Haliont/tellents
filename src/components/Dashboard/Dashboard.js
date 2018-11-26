6de0c370b9b2acdcfdb38d46e060cfdd80a98b1e
Zemliakov Konstantin <zemliakov.konstantin@gmail.com>
2018-11-26 16:01:50 +0600

    Updated Dashboard component

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
+
 import Skills from '../Skills';
 import Search from '../Search';
+import Header from '../Header';
+
 import PageSwitcher from '../../containers/PageSwitcher';
 
 class Dashboard extends Component {
   render() {
     return (
-      <Switch>
-        <Route path="/dashboard/skills" component={Skills} />
-        <Route path="/dashboard/search" component={Search} />
-        <Route path="/dashboard" component={PageSwitcher} />
-      </Switch>
+      <div className="wrapper">
+        <Header />
+        <div className="content">
+          <div className="container-fluid">
+            <Switch>
+              <Route path="/dashboard/skills" component={Skills} />
+              <Route path="/dashboard/search" component={Search} />
+              <Route path="/dashboard" component={PageSwitcher} />
+            </Switch>
+          </div>
+        </div>
+        <div>Footer</div>
+      </div>
     );
   }
 }
