a326b04bbaf1469b94f740ecbfce9cd55c3fcf37
Zemliakov Konstantin <zemliakov.konstantin@gmail.com>
2018-12-11 15:08:03 +0600

    Implement custom link ListItemLink

---
+++ src/components/ListItemLink/ListItemLink.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Route, Link } from 'react-router-dom';
+
+function ListItemLink({
+  to,
+  liProps,
+  children,
+  activeClassName,
+  activeOnlyWhenExact,
+  ...rest
+}) {
+  return (
+    <Route
+      path={to}
+    >{({ match }) => (
+      <li className={match ? activeClassName : ''} {...liProps}>
+        <Link to={to} {...rest}>{children}</Link>
+      </li>
+    )}
+    </Route>
+  );
+}
+
+ListItemLink.defaultProps = {
+  liProps: {},
+  activeClassName: 'active',
+  activeOnlyWhenExact: false,
+};
+
+ListItemLink.propTypes = {
+  to: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  liProps: PropTypes.instanceOf(Object),
+  activeClassName: PropTypes.string,
+  activeOnlyWhenExact: PropTypes.bool,
+};
+
+export default ListItemLink;
