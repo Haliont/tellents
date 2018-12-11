bb95cbe1719802d2d580bc35d75e4c15ed2b195c
Zemliakov Konstantin <zemliakov.konstantin@gmail.com>
2018-12-11 17:17:28 +0600

    Fix display username when user sign up

--- src/store/user/selectors.js
+++ src/store/user/selectors.js
@@ -1,8 +1,13 @@
-// import { createSelector } from 'reselect';
+import { createSelector } from 'reselect';
 
-export const getUsername = state => state.user.userData.full_name;
-export const getUserAvatar = state => (state.user.userData.image || { url: '' }).url;
 export const getUserData = state => state.user.userData;
+
+export const getUsername = createSelector(
+  getUserData,
+  userData => `${userData.first_name} ${userData.last_name}`,
+);
+
+export const getUserAvatar = state => (state.user.userData.image || { url: '' }).url;
 
 export const isSignedIn = state => state.user.isSignedIn;
 export const isSigningIn = state => state.user.signingInState === 'requested';
